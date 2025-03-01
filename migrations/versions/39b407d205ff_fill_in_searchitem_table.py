8bdd71af99bf08181c18f20fd0ea6817ccdc608c
Jana Karausheva <yana-karausheva@yandex.ru>
2025-03-02 00:42:32 +0300

    fixed hanging migration

--- migrations/versions/39b407d205ff_fill_in_searchitem_table.py
+++ migrations/versions/39b407d205ff_fill_in_searchitem_table.py
@@ -9,6 +9,8 @@ Create Date: 2025-01-18 23:33:32.370900
 import sqlalchemy as sa
 from alembic import op
 
+from bot import models
+
 # revision identifiers, used by Alembic.
 revision = '39b407d205ff'
 down_revision = 'bcfedcd90fa6'
@@ -17,8 +19,7 @@ down_revision = 'bcfedcd90fa6'
 def upgrade() -> None:
     """Create searchitem objects for existing users."""
     bind = op.get_bind()
-    users_tbl = sa.Table('users', sa.MetaData(), autoload_with=bind.engine)
-    query = sa.select(users_tbl)
+    query = sa.select(models.User.__table__)
     users = bind.execute(query).all()
     searchitems = [
         {
@@ -28,11 +29,9 @@ def upgrade() -> None:
         }
         for user in users
     ]
-    searchitems_tbl = sa.Table('searchitems', sa.MetaData(), autoload_with=bind.engine)
-    bind.execute(sa.insert(searchitems_tbl), searchitems)
+    bind.execute(sa.insert(models.SearchItem.__table__), searchitems)
 
 
 def downgrade() -> None:
     bind = op.get_bind()
-    searchitems_tbl = sa.Table('searchitems', sa.MetaData(), autoload_with=bind.engine)
-    bind.execute(sa.delete(searchitems_tbl))
+    bind.execute(sa.delete(models.SearchItem.__table__))
